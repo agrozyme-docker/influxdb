b2aa0d35277f63196ce728a503da41c696f76f0d
agrozyme <agrozyme@gmail.com>
2019-01-26 23:36:47 +0800

    update

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Summary
 InfluxDB is an open source time series database for recording metrics, events, and analytics.
 
-# Enviroment Variables
-See: https://docs.influxdata.com/influxdb/v1.5/administration/config/#influxdb-environment-variables-influxdb
-
 # Settings
 - Ports: 8086
 - Data Directory: /var/lib/influxdb
 - Default Configuration File: /etc/influxdb/influxdb.conf
+
+# Enviroment Variables
+See: https://docs.influxdata.com/influxdb/v1.5/administration/config/#influxdb-environment-variables-influxdb
+
+## DOCKER_INFLUXDB_DATABASE
+This variable is optional and allows you to specify the name of a database to be created on image startup.
--- rootfs/usr/local/bin/docker-run.lua
+++ rootfs/usr/local/bin/docker-run.lua
@@ -1,9 +1,39 @@
 #!/usr/bin/lua
 local core = require("docker-core")
 
+local function create_database()
+  local database = core.getenv("DOCKER_INFLUXDB_DATABASE", "")
+
+  if ("" ~= database) then
+    core.run("influx -execute 'CREATE DATABASE %s' ", database)
+  end
+end
+
+local function setup_database()
+  local count = 30
+  local command = string.format("influx -execute 'SHOW DATABASES' ")
+  core.run("su-exec core influxd &")
+
+  while (0 < count) do
+    os.execute("sleep 1")
+
+    if (os.execute(command)) then
+      break
+    end
+
+    core.warn("InfluxDB init process in progress... ")
+    count = count - 1
+  end
+
+  create_database()
+  core.run("killall influxd")
+  core.run("sleep 1")
+end
+
 local function main()
   core.update_user()
-  core.mkdir("/var/lib/influxdb")
+  core.mkdir("/run/influxdb", "/var/lib/influxdb")
+  setup_database()
   core.run("su-exec core influxd")
 end
 
